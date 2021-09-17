3fca3aecef76519f40778247c5569c9d0cd8571b
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2021-09-17 14:49:25 +0000

    Update cheat.js

--- cheat.js
+++ cheat.js
@@ -3,11 +3,10 @@ if(isNaN(wpm)) return alert("Are you sure you entered a number? Try again please
 if(wpm > 150) if(!confirm("Are you sure you want your wpm that high? This could cause you to possibly be detected for cheating or cause you to lag.")) return alert("Ok, exiting.")
 if(wpm <= 0) return alert("Your WPM cant be that low!");
 var i = 0;
-function typingLoop(){
-i++
-if(i == core.text.length+1) return;
+var lastIndex = -1;
+var cheat = setInterval( () => {
+if(lastIndex !== core.cur_char_index ) i++
+if(i == core.text.length+1) clearInterval(cheat);
 var wordLength = core.words[core.cur_word_index].char_list.length;
 core.bound_keypress_handler({key: core.cur_char.chr});
-setTimeout(typingLoop, (11500)/wpm)
-}
-typingLoop()
+}, (12000)/wpm)
